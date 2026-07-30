3ccf4381db1d1554e219cacb5387506de06941e5
marioaje <marioaje@gmail.com>
2026-07-30 12:07:39 -0600

    get js jquery

--- front/js/cursos.js
+++ front/js/cursos.js
@@ -13,8 +13,35 @@ function consultarCurso() {
         url: apiUrl,
         dataType: "json",
         success: function (response) {
-            console.log(response);
+            dibujarTabla(response);
+        },
+        error: function (error) {
+            console.error("Error al consultar los cursos:", error);
         }
     });
+
+}
+
+
+
+function dibujarTabla(cursosDatos) {
+
+    let tabla = $("#tablaCursos");
+    tabla.empty();
+
+
+    $.each(cursosDatos, function (index, cursoElemento) {
+        let fila = `
+            <tr class="">
+                <td scope="row">${cursoElemento._id}</td>
+                <td>${cursoElemento.nombre}</td>
+                <td>${cursoElemento.descripcion}</td>
+                <td>${cursoElemento.estado}</td>
+            </tr>
+        `;
+
+        tabla.append(fila);
+    });
+
 
 }

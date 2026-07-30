b50ae4088448e10c0eebaa60bc999d1c059f9787
marioaje <marioaje@gmail.com>
2026-07-30 13:41:23 -0600

    eliminar y actualizar jquery

--- front/js/cursos.js
+++ front/js/cursos.js
@@ -13,7 +13,16 @@ $(document).ready(function () {
     });
 
 
+    $("#formCursoEditar").submit(function (e) {
+        e.preventDefault();
+        editarCurso();
+    });
 
+    $("#formCursoEliminar").submit(function (e) {
+        e.preventDefault();
+
+        eliminarCurso();
+    });
 
 });
 
@@ -44,6 +53,24 @@ function dibujarTabla(cursosDatos) {
     $.each(cursosDatos, function (index, cursoElemento) {
         let fila = `
             <tr class="">
+                <td>
+                  <a
+                    name=""
+                    id=""
+                    class="btn btn-success"
+                    href="#"
+                    role="button"
+                    onclick="cargarActualizar( '${cursoElemento._id}', '${cursoElemento.nombre}', '${cursoElemento.descripcion}', '${cursoElemento.estado}')"
+                    >Actualizar</a>    
+                    <a
+                    name=""
+                    id=""
+                    class="btn btn-danger"
+                    href="#"
+                    role="button"
+                    onclick="cargarEliminar( '${cursoElemento._id}', '${cursoElemento.nombre}', '${cursoElemento.descripcion}', '${cursoElemento.estado}')"
+                    >Eliminar</a>                  
+                </td>
                 <td scope="row">${cursoElemento._id}</td>
                 <td>${cursoElemento.nombre}</td>
                 <td>${cursoElemento.descripcion}</td>
@@ -71,7 +98,7 @@ function crearCurso() {
     //   "estado": "test d"
     // }
 
-    console.log(JSON.stringify(objetoCursosModel));
+    // console.log(JSON.stringify(objetoCursosModel));
 
     $.ajax({
         type: "POST",
@@ -80,8 +107,7 @@ function crearCurso() {
         data: JSON.stringify(objetoCursosModel),
         contentType: "application/json",
         success: function (response) {
-            alert("Curso creado");
-            consultarCurso();
+            finalizarCurso();
         },
         error: function (error) {
             console.error("Error al crear el curso:", error);
@@ -91,3 +117,114 @@ function crearCurso() {
 
 
 }
+
+function finalizarCurso() {
+    alert("Curso creado");
+    consultarCurso();
+
+    $("#formCursoCrear")[0].reset();
+
+    bootstrap.Modal.getInstance($("#modalId")[0]).hide();
+
+}
+
+
+window.cargarActualizar = function (_id, nombre, descripcion, estado) {
+    $("#idEditar").val(_id);
+    $("#nombreEditar").val(nombre);
+    $("#descripcionEditar").val(descripcion);
+    $("#estadoEditar").val(estado);
+
+
+    const modalEditar = new bootstrap.Modal($("#modalEditar")[0]);
+    modalEditar.show();
+
+}
+
+
+function editarCurso() {
+
+    const idEditar = $("#idEditar").val();
+    const nombre = $("#nombreEditar").val();
+    const descripcion = $("#descripcionEditar").val();
+    const estado = $("#estadoEditar").val();
+
+
+    const objetoCursosModel = new cursosModel(nombre, descripcion, estado);
+
+
+    $.ajax({
+        type: "PUT",
+        url: `${apiUrl}/${idEditar}`,
+        dataType: "json",
+        data: JSON.stringify(objetoCursosModel),
+        contentType: "application/json",
+        success: function (response) {
+            finalizarCursoEditar();
+        },
+        error: function (error) {
+            console.error("Error al editar el curso:", error);
+        }
+    });
+
+
+}
+
+
+
+function finalizarCursoEditar() {
+    alert("Curso editado");
+    consultarCurso();
+
+    $("#formCursoEditar")[0].reset();
+
+    bootstrap.Modal.getInstance($("#modalEditar")[0]).hide();
+
+}
+
+
+
+window.cargarEliminar = function (_id, nombre, descripcion, estado) {
+    $("#idEliminar").val(_id);
+    $("#nombreEliminar").val(nombre);
+    $("#descripcionEliminar").val(descripcion);
+    $("#estadoEliminar").val(estado);
+
+
+    const modalEliminar = new bootstrap.Modal($("#modalEliminar")[0]);
+    modalEliminar.show();
+
+}
+
+
+function eliminarCurso() {
+
+    const idEliminar = $("#idEliminar").val();
+
+
+    $.ajax({
+        type: "DELETE",
+        url: `${apiUrl}/${idEliminar}`,
+        dataType: "json",
+        contentType: "application/json",
+        success: function (response) {
+            finalizarCursoEliminar();
+        },
+        error: function (error) {
+            console.error("Error al eliminar el curso:", error);
+        }
+    });
+
+}
+
+
+function finalizarCursoEliminar() {
+    alert("Curso eliminado");
+    consultarCurso();
+
+    $("#formCursoEliminar")[0].reset();
+
+    bootstrap.Modal.getInstance($("#modalEliminar")[0]).hide();
+
+}
+
